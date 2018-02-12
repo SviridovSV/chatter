34663b0782fa26200ef2a241603b9b67b1d4ca31
Sergey <sviridov.sv82@gmail.com>
2018-02-12 13:41:55 +0200

    create post

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 
-  root :to => 'users#index'
+  root :to => 'posts#index'
   resources :user_sessions
   resources :users
+  resources :posts
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
